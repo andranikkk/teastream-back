7754345d78b6002f6c252da2b432074d01e6f38c
Andranik Aivazyan <ayvazyanandranik46@gmail.com>
2026-01-06 22:51:48 +0400

    feat: implemented email and password changing functionality

--- src/modules/auth/account/account.resolver.ts
+++ src/modules/auth/account/account.resolver.ts
@@ -1,9 +1,12 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import type { User } from '@prisma/client'
 
 import { AuthDecoration } from '@/src/shared/decorators/auth.decorator'
 import { Authorized } from '@/src/shared/decorators/authorized.decorator'
 
 import { AccountService } from './account.service'
+import { ChangeEmailInput } from './inputs/change-email.input'
+import { ChangePasswordInput } from './inputs/change-password.input'
 import { CreateUserInput } from './inputs/create-user.input'
 import { UserModel } from './models/user.model'
 
@@ -16,8 +19,27 @@ export class AccountResolver {
 	public async me(@Authorized('id') id: string) {
 		return this.accountService.me(id)
 	}
+
 	@Mutation(() => Boolean, { name: 'createUser' })
 	public async create(@Args('data') input: CreateUserInput) {
 		return this.accountService.create(input)
+	}
+
+	@AuthDecoration()
+	@Mutation(() => Boolean, { name: 'changeEmail' })
+	public async changeEmail(
+		@Args('data') input: ChangeEmailInput,
+		@Authorized() user: User
+	) {
+		return this.accountService.changeEmail(user, input)
+	}
+
+	@AuthDecoration()
+	@Mutation(() => Boolean, { name: 'changePassword' })
+	public async changePassword(
+		@Args('data') input: ChangePasswordInput,
+		@Authorized() user: User
+	) {
+		return this.accountService.changePassword(user, input)
 	}
 }
--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@nestjs/common'
-import { hash } from 'argon2'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import type { User } from '@prisma/client'
+import { hash, verify } from 'argon2'
 
 import { PrismaService } from '../../../core/prisma/prisma.service'
 import { VerificationService } from '../verification/verification.service'
 
+import { ChangeEmailInput } from './inputs/change-email.input'
+import { ChangePasswordInput } from './inputs/change-password.input'
 import { CreateUserInput } from './inputs/create-user.input'
 
 @Injectable()
 export class AccountService {
 	public constructor(
 		private readonly prismaService: PrismaService,
-		private readonly verificationService: VerificationService //** TO ENABLE EMAIL VERIFICATION FOR REGISTRATION */
+		private readonly verificationService: VerificationService
 	) {}
 
 	public async me(id: string) {
@@ -52,6 +55,34 @@ export class AccountService {
 		})
 
 		await this.verificationService.sendVerificationToken(user)
+
+		return true
+	}
+
+	public async changeEmail(user: User, input: ChangeEmailInput) {
+		const { email } = input
+
+		await this.prismaService.user.update({
+			where: { id: user.id },
+			data: { email }
+		})
+
+		return true
+	}
+
+	public async changePassword(user: User, input: ChangePasswordInput) {
+		const { currentPassword, newPassword } = input
+
+		const isValidPassword = await verify(user.password, currentPassword)
+
+		if (!isValidPassword) {
+			throw new UnauthorizedException('Password is incorrect')
+		}
+
+		await this.prismaService.user.update({
+			where: { id: user.id },
+			data: { password: await hash(newPassword) }
+		})
 
 		return true
 	}
