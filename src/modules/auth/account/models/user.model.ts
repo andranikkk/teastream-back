294ab1893353645091573bb06af85a5a22992471
Andranik Aivazyan <ayvazyanandranik46@gmail.com>
2026-05-02 19:17:12 +0400

    feat: channel entity/features initialization

--- src/modules/auth/account/models/user.model.ts
+++ src/modules/auth/account/models/user.model.ts
@@ -1,6 +1,7 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql'
 import type { User } from '@prisma/client'
 
+import { FollowModel } from '@/src/modules/follow/models/follow.model'
 import { StreamModel } from '@/src/modules/stream/models/stream.model'
 
 import { SocialLinkModel } from '../../profile/models/social-link.model'
@@ -49,8 +50,14 @@ export class UserModel implements User {
 	@Field(() => [SocialLinkModel])
 	socialLinks: SocialLinkModel[]
 
-	@Field(() => StreamModel)
-	stream: StreamModel
+	@Field(() => [FollowModel])
+	following: FollowModel[]
+
+	@Field(() => [FollowModel])
+	follower: FollowModel[]
+
+	@Field(() => StreamModel, { nullable: true })
+	streams: StreamModel
 
 	@Field(() => Date)
 	createdAt: Date
